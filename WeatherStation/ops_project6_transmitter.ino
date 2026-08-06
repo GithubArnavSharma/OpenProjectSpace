7054513c987d6672938e183b8b3241ee9e0e43c1
Arnav Sharma <77365987+GithubArnavSharma@users.noreply.github.com>
2026-08-05 17:12:42 -0700

    Create ops_project6_transmitter.ino

---
+++ WeatherStation/ops_project6_transmitter.ino
@@ -0,0 +1,54 @@
+#include <WiFiConfig.h>
+#include <Adafruit_AHTX0.h>
+#include <Wire.h>
+//Find the mac address of your receiver ESP32 first, then enter it in here:
+uint8_t receiverAddress[] = {0xA8, 0x46, 0x74, 0x5C, 0x19, 0x2C};
+
+WeatherReport report;      // global initialization of WeatherReport
+
+
+#define I2C_SDA 6
+#define I2C_SCL 7
+const int PR_PIN = 4;
+sensors_event_t humidity, temp;
+Adafruit_AHTX0 aht;
+
+
+void setup()
+{
+ Serial.begin(115200);                // Serial setup
+
+
+ Wire.begin(I2C_SDA, I2C_SCL);
+ aht.begin();
+
+
+ pinMode(PR_PIN, INPUT);
+
+
+ wifi_setup();                      // start wifi mode and ESP-NOW
+ peer_setup(receiverAddress);       // configure receiving address
+ //set_report_receiver(&report);      // configure the receiver ESP32 to update report’s values upon reception
+}
+
+
+void loop()
+{
+ aht.getEvent(&humidity, &temp);
+ report.temperature = temp.temperature;
+ report.humidity = humidity.relative_humidity;
+
+
+ int light = analogRead(PR_PIN);
+ Serial.println(light);
+ report.light = 0;
+ if (light > 1000) {
+   report.light = 1;
+ }
+
+
+ send_report(receiverAddress, report);       // send the report
+
+
+ delay(10000);
+}
